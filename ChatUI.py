2acb6972c24cf107ca17238e88d2abbb9bf7f953
venkata hemanth <venkatahemanth135@gmail.com>
2025-08-04 08:14:13 +0000

    made as chatbot

--- ChatUI.py
+++ ChatUI.py
@@ -2,10 +2,6 @@ import chainlit as cl
 from typing import List
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
-
 
 def get_prompt(instruction: str, history: List[str] = None) -> str:
     system = "You are an AI assistant who answers in very short and precisely"
@@ -19,8 +15,20 @@ def get_prompt(instruction: str, history: List[str] = None) -> str:
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = f"Hello, you just sent: {message.content}!"
+    #    response = f"Hello, you just sent: {message.content}!"
+    prompt = get_prompt(message.content)
+    response = llm(prompt)
     await cl.Message(response).send()
+
+
+@cl.on_chat_start
+def on_chat_start():
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+    )
+    print("A new chat session has started!")
+
 
 """
 history = []
